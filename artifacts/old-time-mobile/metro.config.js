3bdd3f7613ae68f9bed33e55e800e06404c7b977
oldtimemessenger <monroecpr@gmail.com>
2026-09-11 12:21:43 -0400

    Fix mobile native startup crash

--- artifacts/old-time-mobile/metro.config.js
+++ artifacts/old-time-mobile/metro.config.js
@@ -6,12 +6,12 @@ const workspaceRoot = path.resolve(projectRoot, '../..');
 const apiClientRoot = path.resolve(workspaceRoot, 'lib/api-client-react');
 const config = getDefaultConfig(projectRoot);
 
-config.watchFolders = [apiClientRoot];
+config.watchFolders = [...config.watchFolders, apiClientRoot];
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
   path.resolve(workspaceRoot, 'node_modules'),
 ];
-config.resolver.disableHierarchicalLookup = true;
+config.resolver.disableHierarchicalLookup = false;
 config.resolver.extraNodeModules = {
   react: path.resolve(workspaceRoot, 'node_modules/react'),
   'react-native': path.resolve(workspaceRoot, 'node_modules/react-native'),
